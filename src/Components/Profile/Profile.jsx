43b52122268c3f678d8ef7dfae796e1270b621ec
Nazeem-c <mhdnazeemc@gmail.com>
2024-02-22 18:11:48 +0530

    student portal

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -19,56 +19,37 @@ const Profile = () => {
   return (
     <div className={styles.container}>
       <div className={`${styles.warpper} flexColStart paddings innerWidth`}>
-        <div className={`${styles.topbanner} flexStart`}>
-          <div className={`${styles.left} flexColStart paddings`}>
+        <div className={`${styles.topbanner} flexStart paddings`}>
+          <div className={`${styles.left} flexColStart `}>
             <div className={`${styles.top} flexStart`}>
               <p className={styles.primary}>{currentDate.toDateString()}</p>
             </div>
             <div className="flexColStart">
               <p className={styles.secondary}>Welcome Back Jhon Doe</p>
               <p className={styles.tert}>
-                {" "}
                 Always stay updated in your student portal
               </p>
             </div>
           </div>
+          <div className={`${styles.detailscontainer} flexColStart`}>
+            <p className={`${styles.headtext}`}>namecccccccccs</p>
+            <p className={`${styles.subtext}`}>id</p>
+            <br />
+            <p className={`${styles.textcontent}`}>Department : </p>
+            <p className={`${styles.textcontent}`}>College : </p>
+            <p className={`${styles.textcontent}`}>College ID : </p>
+            <p className={`${styles.textcontent}`}>Batch : </p>
+            <p className={`${styles.textcontent}`}>Mail : </p>
+          </div>
           <div className={`${styles.right} innerWidth`}>
             <img
               className={styles.img}
-              src={process.env.PUBLIC_URL + "/Assets/banner.png"}
+              src={process.env.PUBLIC_URL + "/Assets/profile.png"}
               alt="banner"
             />
           </div>
         </div>
-        <div className={styles.details}>
-          <div className={`${styles.detailswrapp}`}>
-            <div className={`${styles.maindetaails} flexStart`}>
-              <div className={`${styles.center}`}>
-                <div className={`${styles.profile} flexCenter`}>
-                  <div className={`${styles.profilephoto}`}>
-                    <img
-                      src={process.env.PUBLIC_URL + "/Assets/profile.png"}
-                      alt=""
-                    />
-                  </div>
-                  <div className={`${styles.data} flexColStart`}>
-                    <p className={`${styles.headtext}`}>namecccccccccs</p>
-                    <p className={`${styles.subtext}`}>id</p>
-                  </div>
-                </div>
-              </div>
-
-              <div className={`${styles.detailscontainer} flexColStart`}>
-                <p>Department : </p>
-                <p>College : </p>
-                <p>College ID : </p>
-                <p>Batch : </p>
-                <p>Mail : </p>
-              </div>
-            </div>
-            <h1>gcbhjxn</h1>
-          </div>
-        </div>
+       
       </div>
     </div>
   );
